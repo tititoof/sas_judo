28f13515dd08c26f4ee7bbe14ca72f3d38bfe67f
Christophe Hartmann <chartmann.35@gmail.com>
2017-07-19 07:45:04 +0200

    Quality CoursesRepository

--- app/Repositories/CoursesRepository.php
+++ app/Repositories/CoursesRepository.php
@@ -11,6 +11,15 @@ use Illuminate\Http\Request;
 
 class CoursesRepository
 {
+    /**
+     * 
+     */
+    const WEEK_DAYS = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+    
+    const START_AT  = 'start_at';
+    
+    const END_AT    = 'end_at';
+    
     public function save(Request $request)
     {
         return $this->update($request, new Course);
@@ -19,8 +28,8 @@ class CoursesRepository
     public function update(Request $request, Course $course)
     {
         try {
-            $startAt            = $this->setDateTime(new \DateTime('now'), $request->input('start_at'));
-            $endAt              = $this->setDateTime(new \DateTime('now'), $request->input('end_at'));
+            $startAt            = $this->setDateTime(new \DateTime('now'), $request->input(self::START_AT));
+            $endAt              = $this->setDateTime(new \DateTime('now'), $request->input(self::END_AT));
             $course->name       = $request->input('name');
             $course->day        = $request->input('day');
             $course->start_at   = $startAt->format('H:i');
@@ -63,30 +72,29 @@ class CoursesRepository
     public function getCourses()
     {
         $courses = Course::all();
-        $courses = $courses->map(function($course, $key) {
+        return $courses->map(function($course) {
             return [
                 'id'        => $course->id,
                 'season'    => $course->season->name,
                 'day'       => $course->day,
-                'start_at'  => $course->start_at,
-                'end_at'    => $course->end_at,
+                self::START_AT  => $course->start_at,
+                self::END_AT    => $course->end_at,
                 'teacher'   => $course->teacher->name,
                 ];
             });
-        return $courses;
     }
 
     public function getSchedulerCourses()
     {
         $year   = Carbon::now();
-        $season = Season::where('start_at', '<=', $year)
-                        ->where('end_at', '>=', $year)
+        $season = Season::where(self::START_AT, '<=', $year)
+                        ->where(self::END_AT, '>=', $year)
                         ->first()
                         ->get();
         $yearCourses = Course::where('season_id', '=', $season[0]->id)->get();
-        $days = collect(['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']);
-        $courses = $days->map(function($day, $key) use ($yearCourses) {
-            return $yearCourses->map(function($item, $index) use ($day) {
+        $days = collect(WEEK_DAYS);
+        return $days->map(function($day) use ($yearCourses) {
+            return $yearCourses->map(function($item) use ($day) {
                 $course = new \stdClass();
                 if ($day == $item->day) {
                     $course->dateStart  = $item->start_at;
@@ -96,15 +104,13 @@ class CoursesRepository
                 return $course;
             });
         });
-        return $courses;
     }
 
     private function getDays()
     {
-        $days = collect(['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']);
-        $days = $days->map(function($day, $key) {
+        $days = collect(WEEK_DAYS);
+        return $days->map(function($day, $key) {
             return ['label' => $day, 'value' => $day];
         });
-        return $days;
     }
 }
