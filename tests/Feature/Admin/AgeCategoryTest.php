7436ec289e0707eef811f820d1a2fa0446302867
Christophe Hartmann <c.hartmann@vermon.com>
2017-09-20 14:36:17 +0200

    Beginning age categories tests

--- tests/Feature/Admin/AgeCategoryTest.php
+++ tests/Feature/Admin/AgeCategoryTest.php
@@ -8,11 +8,34 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Database\Eloquent\Collection;
 use App\Models\User;
+use App\Models\AgeCategory;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AgeCategoryTest extends TestCase
 {
+    /**
+     * User for test
+     */
+    private $user  = null;
+
+    /**
+     * token for user
+     */
     private $token = null;
+
+    /**
+     * age categories for testing
+     */
+    private $ageCategories = null;
+
+    public function setupTests()
+    {
+        if (null === $this->user) {
+            $this->user          = factory(User::class, 'admin')->create();
+            $this->ageCategories = factory(AgeCategory::class, 5)->make();
+            $this->signIn(['email' => $this->user->email, 'password' => 'secret']);
+        }
+    }
 
     public function signIn($data = [ 'email'=>'youremail@yahoo.com', 'password'=>'password' ])
     {
@@ -26,9 +49,50 @@ class AgeCategoryTest extends TestCase
 
     public function testIndexStatus()
     {
-        $user       = factory(User::class, 'admin')->create();
-        $this->signIn(['email' => $user->email, 'password' => 'secret']);
+        $this->setupTests();
         $response   = $this->get('/api/age_category', [ 'Authorization' => "Bearer ".(string)($this->token) ]);
         $response->assertStatus(200);
+    }
+
+    public function testCreateStatus()
+    {
+        $this->setupTests();
+        foreach ($this->ageCategories as $ageCategory) {
+            $response = $this->json(
+                'POST',
+                'api/age_category',
+                [
+                    'name'          => $ageCategory->name,
+                    'years'         => $ageCategory->years,
+                ],
+                [
+                    'Authorization' => "Bearer ".(string)($this->token),
+                ]
+            );
+            $response->assertStatus(200);
+        }
+    }
+
+    public function testUpdateStatus()
+    {
+        $this->setupTests();
+        foreach ($this->ageCategories as $ageCategory) {
+            $response = $this->json(
+                'POST',
+                'api/age_category/'.$ageCategory->id,
+                [
+                    'name'          => $ageCategory->name,
+                    'years'         => ($ageCategory->years - 10),
+                ],
+                [
+                    'Authorization' => "Bearer ".(string)($this->token),
+                ]
+            );
+            $response->assertStatus(200);
+            $this->assertDatabaseHas('age_categories', [
+                'name'          => $ageCategory->name,
+                'years'         => ($ageCategory->years - 10),
+            ]);
+        }
     }
 }
