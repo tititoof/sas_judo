9bfdb47b3114eb5df1dc673460ba6b4f067f8fc8
Christophe Hartmann <chartmann.35@gmail.com>
2017-09-22 07:36:21 +0200

    Add edit / update / destroy user tests

--- tests/Feature/Admin/UserTest.php
+++ tests/Feature/Admin/UserTest.php
@@ -81,5 +81,61 @@ class UserTest extends TestCase
             'is_debug'      => 1
         ]);
     }
+    
+    public function testEditStatus()
+    {
+        $this->setupTests();
+        $response = $this->json(
+            'GET',
+            'api/admin/user/'.$this->user->id.'/edit',
+            [],
+            ['Authorization' => "Bearer ".(string)($this->token)]
+        );
+        $response->assertStatus(200);
+    }
+    
+    public function testUpdateDB()
+    {
+        $this->setupTests();
+        $firstnameChanged   = $this->user->firstname.' changed';
+        $lastnameChanged    = $this->user->lastname.' changed';
+        $response           = $this->json(
+            'PUT',
+            'api/admin/user/'.$this->user->id,
+            [
+                'firstname'     => $firstnameChanged,
+                'lastname'      => $lastnameChanged,
+                'email'         => $this->user->email,
+                'address'       => $this->user->address,
+                'phone'         => $this->user->phone,
+                'is_admin'      => $this->user->is_admin,
+                'is_debug'      => $this->user->is_debug,
+                'is_teacher'    => $this->user->is_teacher,
+            ],
+            ['Authorization' => "Bearer ".(string)($this->token)]
+        );
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('users', [
+            'firstname'     => $firstnameChanged,
+            'lastname'      => $lastnameChanged,
+            'id'            => $this->user->id,
+        ]);
+    }
+    
+    public function testDestroyStatus()
+    {
+        $this->setupTests();
+        $user = User:all()->last();
+        $response = $this->json(
+            'DELETE',
+            'api/admin/user/'.$user->id,
+            [],
+            ['Authorization' => "Bearer ".(string)($this->token)]
+        );
+        $response->assertStatus(200);
+        $this->assertDatabaseMissing('users', [
+            'id'    => $user->id
+        ]);
+    }
 
 }
