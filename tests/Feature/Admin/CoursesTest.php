29bbafc3b5cc188b0e104700aa2a2ce7be64c42f
Christophe Hartmann <chartmann.35@gmail.com>
2017-09-23 12:10:29 +0200

    Add create / store courses tests

--- tests/Feature/Admin/CoursesTest.php
+++ tests/Feature/Admin/CoursesTest.php
@@ -32,4 +32,40 @@ class CoursesTest extends TestCase
         $response = $this->get('/api/course', [ 'Authorization' => "Bearer ".(string)($this->token) ]);
         $response->assertStatus(200);
     }
+    
+    public function testCreateAction()
+    {
+        $this->setupTests();
+        $response = $this->json(
+            'GET', 
+            '/api/course/create',
+            [],
+            [ 'Authorization' => "Bearer ".(string)($this->token) ]
+        );
+    }
+    
+    public function testStoreAction()
+    {
+        $this->setupTests();
+        $this->course   = factory(Course::class)->make();
+        $startAt        = explode(':', $this->course->start_at);
+        $endAt          = explode(':', $this->course->end_at);
+        $response       = $this->json(
+            'POST',
+            '/api/course',
+            [
+                'name'          => $this->course->name,
+                'season_id'     => $this->course->season_id,
+                'day'           => $this->course->day,
+                'start_at'      => ['HH' => $startAt[0], 'mm' => $startAt[1]],
+                'end_at'        => ['HH' => $endAt[0], 'mm' => $endAt[1]],
+                'teacher_id'    => $this->course->teacher_id,
+            ],
+            [ 'Authorization' => "Bearer ".(string)($this->token) ]
+        );
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('courses', [
+            'name'    => $this->course->name,
+        ]);
+    }
 }
