790fc5406f77830ae1993d54bba475df7bb9589f
Christophe Hartmann <chartmann.35@gmail.com>
2017-12-23 17:07:56 +0100

    Changing

--- app/Http/Requests/CourseFormRequest.php
+++ app/Http/Requests/CourseFormRequest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class CourseFormRequest extends FormRequest
 {
-    CONST REQUIRED = 'required';
+    const REQUIRED = 'required';
 
     /**
      * Determine if the user is authorized to make this request.
@@ -27,7 +27,7 @@ class CourseFormRequest extends FormRequest
     {
         $course    = (null !== $this->route('course')) ? $this->route('course') : null;
         $rules = [
-            'name'          => 'required|max:255', //|unique:courses,name',
+            'name'          => 'required|max:255',
             'season_id'     => self::REQUIRED,
             'day'           => self::REQUIRED,
             'start_at'      => self::REQUIRED,
@@ -35,9 +35,9 @@ class CourseFormRequest extends FormRequest
             'teacher_id'    => self::REQUIRED,
             'color'         => self::REQUIRED,
         ];
-        // if (null !== $course) {
-        //     $rules = array_merge($rules, ['name' => 'required|max:255|unique:courses,name,'.$course['attributes']['id'],]);
-        // }
+        if (null !== $course) {
+            $rules = array_merge($rules, ['name' => 'required|max:255|unique:courses,name,'.$course['attributes']['id'],]);
+        }
         return $rules;
     }
 }
